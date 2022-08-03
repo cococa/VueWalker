f21ad49deb1ac86a59dddc334894323df3cf4118
沈军 <shenjun@qccr.com>
2022-08-03 20:16:46 +0800

    add

--- src/main.js
+++ src/main.js
@@ -1,27 +1,46 @@
 let activeEffect;
 
 function effect(fn) {
-  activeEffect = fn;
-  fn();
+  const effectFn = ()=>{
+    cleanup(effectFn)
+    activeEffect = effectFn;
+    fn();
+  }
+  effectFn.deps = []
+  effectFn()
+}
+
+function cleanup(effectFn){
+  for(var i = 0; i< effectFn.deps.length; i++){
+      const deps = effectFn.deps[i];
+      deps.delete(effectFn)
+  }
+  effectFn.deps.length = 0
 }
 
 
-/**
- *  这里注释下：
- *  WeakMap的 key 是 Proxy 的代理对象（对应下面代码中的 target ），value 则是 Map 
- *  Map 中的 key 是代理对象的属性，value 则是一个 Set , 存放与代理对象的属性绑定的副作用函数集合
- * 
- *  
- * 
- * 
- *  Q:
- *   1. WeakMap 的 key 用的是一个对象，怎么确保唯一性？ 
- *   2.  
- * 
- */
+
+
+function track(target ,key) {
+  if(!activeEffect){return }
+  let depsMap = bucket.get(target)
+  if(!depsMap){
+    bucket.set(target, depsMap = new Map())
+  }
+  let deps = depsMap.get(key)
+  if(!deps){
+    depsMap.set(key, deps = new Set())
+  }
+  deps.add(activeEffect)
+  activeEffect.deps.push(deps)
+}
+
+
+
+
 const bucket = new WeakMap();
 
-const data = { text: "123" };
+const data = { ok : false , text: "123" };
 
 const obj = new Proxy(data, {
   get(target, prop) {
@@ -57,22 +76,9 @@ const obj = new Proxy(data, {
 // testcode 1
 effect(() => {
   console.log("do effect");
-  document.body.innerHTML = obj.text;
+  document.body.innerHTML = obj.ok ? obj.text : 'not';
 });
 setTimeout(() => {
-  obj.text = "Hello Vue";
+  obj.ok  = true
 }, 3000);
 
-
-// testcode 2
-// effect(() => {
-//     console.log("do effect");
-//     document.body.innerHTML = obj.otherText;
-//   });
-// setTimeout(() => {
-//     console.log("setTimeout change otherText");
-//     obj.otherText = "Hello Vue";
-// }, 3000);
-// setTimeout(() => {
-//   console.log("bucket", bucket);
-// }, 7000);
