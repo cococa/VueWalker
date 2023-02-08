1912d9b7c36d72971d0ef80903bcfffd43a8340c
cococa <cocoooooa@gmail.com>
2023-02-08 21:09:45 +0800

    update 4.0

--- src/main.js
+++ src/main.js
@@ -1,84 +1,79 @@
 let activeEffect;
 
 function effect(fn) {
-  const effectFn = ()=>{
+  const effectFn = () => {
+    //4.0 新增代码
     cleanup(effectFn)
     activeEffect = effectFn;
     fn();
-  }
-  effectFn.deps = []
-  effectFn()
+  };
+  effectFn.deps = [];
+  effectFn();
 }
 
-function cleanup(effectFn){
-  for(var i = 0; i< effectFn.deps.length; i++){
-      const deps = effectFn.deps[i];
-      deps.delete(effectFn)
+function cleanup(effectFn) {
+  for (var i = 0; i < effectFn.deps.length; i++) {
+    const deps = effectFn.deps[i];
+    deps.delete(effectFn);
   }
-  effectFn.deps.length = 0
+  effectFn.deps.length = 0;
 }
-
-
-
-
-function track(target ,key) {
-  if(!activeEffect){return }
-  let depsMap = bucket.get(target)
-  if(!depsMap){
-    bucket.set(target, depsMap = new Map())
-  }
-  let deps = depsMap.get(key)
-  if(!deps){
-    depsMap.set(key, deps = new Set())
-  }
-  deps.add(activeEffect)
-  activeEffect.deps.push(deps)
-}
-
-
-
 
 const bucket = new WeakMap();
 
-const data = { ok : false , text: "123" };
+const data = { ok: true, text: "this is text props value" };
 
 const obj = new Proxy(data, {
   get(target, prop) {
-    console.log("Proxy get", {target, prop});
-    if (!activeEffect) {
-      return;
-    }
-    let depsMap = bucket.get(target);
-    if (!depsMap) {
-      bucket.set(target, (depsMap = new Map()));
-    }
-    let deps = depsMap.get(prop);
-    if (!deps) {
-      depsMap.set(prop, (deps = new Set()));
-    }
-    deps.add(activeEffect);
+    console.log("Proxy get", { target, prop });
+    track(target, prop);
     return target[prop];
   },
 
   set(target, prop, value) {
-    console.log("Proxy set", {target, prop, value});
+    console.log("Proxy set", { target, prop, value });
     target[prop] = value;
-    let depsMap = bucket.get(target);
-    if (!depsMap) {
-      return;
-    }
-    const fns = depsMap.get(prop);
-    fns && fns.forEach((fn) => fn());
+    trigger(target,prop);
   },
 });
 
+function track(target, key) {
+  if (!activeEffect) {
+    return;
+  }
+  let depsMap = bucket.get(target);
+  if (!depsMap) {
+    bucket.set(target, (depsMap = new Map()));
+  }
+  let deps = depsMap.get(key);
+  if (!deps) {
+    depsMap.set(key, (deps = new Set()));
+  }
+  deps.add(activeEffect);
+  activeEffect.deps.push(deps);
+}
+
+function trigger(target, key) {
+
+  let depsMap = bucket.get(target);
+  if (!depsMap) {
+    return;
+  }
+  const fns = depsMap.get(key);
+  
+  //4.0 新增代码
+  const effectToRun = new Set(fns);
+  effectToRun.forEach(fn => fn());
+  // 3.0 删除的代码
+  // fns && fns.forEach((fn) => fn());
+}
 
 // testcode 1
 effect(() => {
   console.log("do effect");
-  document.body.innerHTML = obj.ok ? obj.text : 'not';
+  document.body.innerHTML = obj.ok ? obj.text : "not";
 });
 setTimeout(() => {
-  obj.ok  = true
+  obj.ok = false;
+  obj.text = 'hello vue3'
 }, 3000);
-
