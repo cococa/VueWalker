52f3121cb511b7e26d437313cb76327579e75b68
cococa <cocoooooa@gmail.com>
2023-03-03 15:33:02 +0800

    add v5.0

--- src/main.js
+++ src/main.js
@@ -1,12 +1,17 @@
 let activeEffect;
+let effectStrack = []
 
 function effect(fn) {
   const effectFn = () => {
     console.log("effectFn", effectFn.deps);
     //4.0 新增代码
-    cleanup(effectFn)
+    cleanup(effectFn);
     activeEffect = effectFn;
     fn();
+    // 当前的副作用函数执行结束后, 出栈
+    effectStrack.pop()
+    // activeEffect 还原为之前的值
+    activeEffect = effectStrack[effectStrack.length - 1]
   };
   effectFn.deps = [];
   effectFn();
@@ -22,7 +27,7 @@ function cleanup(effectFn) {
 
 const bucket = new WeakMap();
 
-const data = { ok: true, text: "this is text props value" };
+const data = { ok: true, text: "this is text props value", count: 0 };
 
 const obj = new Proxy(data, {
   get(target, prop) {
@@ -34,7 +39,7 @@ const obj = new Proxy(data, {
   set(target, prop, value) {
     console.log("Proxy set", { target, prop, value });
     target[prop] = value;
-    trigger(target,prop);
+    trigger(target, prop);
   },
 });
 
@@ -55,30 +60,29 @@ function track(target, key) {
 }
 
 function trigger(target, key) {
-
   let depsMap = bucket.get(target);
+  console.log("start trigger depsMap", depsMap);
   if (!depsMap) {
     return;
   }
   const fns = depsMap.get(key);
-  
-  //4.0 新增代码 防止死循环
-  //在调用 forEach 遍历 Set 集合时，如果一个值已经被访问过了，但该值被删除并重新添加到集合，如果此时 forEach 遍历没有结束，那么该值会重新被访问。因此，上面的代码会无限执行。解决办法很简单，我们可以构造另外一个 Set 集合并遍历它：
-  const effectToRun = new Set(fns);
-  effectToRun.forEach(fn => fn());
-  // 3.0 删除的代码
-  //  fns && fns.forEach((fn) => fn());
+  console.log("start trigger fns", fns);
+  const effectToRun = new Set();
+  //5.0 新增代码 防止死循环
+  fns && fns.forEach((fn) => {
+    if (activeEffect !== fn) {
+      effectToRun.add(fn);
+    }
+  });
+  console.log("start trigger effectToRun", effectToRun);
+  effectToRun.forEach((fn) => fn());
+  console.log("effectStrack",effectStrack);
 }
 
-// testcode 1
 effect(() => {
-  // 3.0 会执行三次，4.0 只会执行两次
   console.log("do effect");
-  document.body.innerHTML = obj.ok ? obj.text : "not";
+  document.body.innerHTML = obj.count;
 });
 setTimeout(() => {
-  obj.ok = false;
-  obj.text = 'hello vue3'
+  obj.count++;
 }, 3000);
-
-
